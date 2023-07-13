ec4a414ae703ec2d08e4e5d64696ef21a4921d44
Sut103 <kou.103.kan@gmail.com>
2023-07-13 14:35:05 +0900

    lambdaを前提として修正

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/Sut103/HCW-SS-Viewer/aws"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/labstack/echo/v4"
+
+	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
 )
+
+var echoLambda *echoadapter.EchoLambda
+
+func init() {
+	e := echo.New()
+	e.GET("/", getScreenshots)
+
+	echoLambda = echoadapter.New(e)
+}
 
 func getScreenshots(c echo.Context) error {
 	screenshots, err := aws.Scan()
@@ -26,10 +40,10 @@ func getScreenshots(c echo.Context) error {
 	return c.JSON(http.StatusOK, string(jsonByte))
 }
 
+func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return echoLambda.ProxyWithContext(ctx, req)
+}
+
 func main() {
-	e := echo.New()
-
-	e.GET("/api/screenshots", getScreenshots)
-
-	e.Logger.Fatal(e.Start(":1323"))
+	lambda.Start(HandleRequest)
 }
