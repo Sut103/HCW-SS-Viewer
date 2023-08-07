9e7e5f0c087d6d24dfa6f5c6b19d8b746bf2bd11
Sut103 <kou.103.kan@gmail.com>
2023-08-07 09:13:28 +0000

    HTMLテンプレートに画像を表示するようにしてみた

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"html/template"
+	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -17,11 +19,24 @@ import (
 
 var echoLambda *echoadapter.EchoLambda
 
+type Template struct {
+	templates *template.Template
+}
+
 func init() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+
 	e := echo.New()
+	e.Renderer = t
 	e.GET("/", getScreenshots)
 
 	echoLambda = echoadapter.New(e)
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func getScreenshots(c echo.Context) error {
@@ -31,13 +46,14 @@ func getScreenshots(c echo.Context) error {
 		return echo.ErrInternalServerError
 	}
 
-	jsonByte, err := json.Marshal(screenshots)
-	if err != nil {
-		log.Println(time.Now(), "getScreenshots(): ", err)
-		return echo.ErrInternalServerError
+	// 暫定で配列からランダムに6個とりだす
+	ret_screenshots := make([]aws.Screenshot, 0)
+	for i := 0; i < 6; i++ {
+		rand.Seed(time.Now().UnixNano())
+		ret_screenshots = append(ret_screenshots, screenshots[rand.Intn(len(screenshots))])
 	}
 
-	return c.JSON(http.StatusOK, string(jsonByte))
+	return c.Render(http.StatusOK, "main", ret_screenshots)
 }
 
 func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
